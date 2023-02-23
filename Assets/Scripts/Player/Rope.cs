5632f4e2892a79304a4178ee6298a8e3f8a13c18
Harits Nur Fauzan <mharitsnf@gmail.com>
2023-02-23 17:00:52 +0100

    Initial rope placement

--- Assets/Scripts/Player/Rope.cs
+++ Assets/Scripts/Player/Rope.cs
@@ -26,6 +26,7 @@ public class Rope : MonoBehaviour
 
     [ReadOnly] public readonly List<EndData> ends = new();
     private LineRenderer _lineRenderer;
+    private bool _foundHit;
 
     private void Start()
     {
@@ -37,9 +38,52 @@ public class Rope : MonoBehaviour
         UpdateDynamicIndexes();
     }
 
+    private void FixedUpdate()
+    {
+        CheckPassingRigidbody();
+    }
+
     private void LateUpdate()
     {
         DrawLine();
+    }
+
+    private void CheckPassingRigidbody()
+    {
+        if (ends.Count < 2) return;
+        if (ends[0].rigidbody || ends[1].rigidbody) return;
+        if (_foundHit) return;
+
+        bool rayCast1 = Physics.Raycast(ends[0].worldPosition, (ends[1].worldPosition - ends[0].worldPosition).normalized,
+            out var hit1,
+            (ends[1].worldPosition - ends[0].worldPosition).magnitude);
+        bool rayCast2 = Physics.Raycast(ends[1].worldPosition, (ends[0].worldPosition - ends[1].worldPosition).normalized,
+            out var hit2,
+            (ends[0].worldPosition - ends[1].worldPosition).magnitude);
+        if (!rayCast1 && !rayCast2) return;
+
+        if (!hit1.rigidbody) return;
+
+        if (hit1.collider.gameObject.CompareTag("Player")) return;
+
+        hit1.rigidbody.freezeRotation = true;
+        
+        EndData newEnd1 = new EndData(hit1.rigidbody, hit1.rigidbody.transform.InverseTransformPoint(hit1.point));
+        ends.Insert(1, newEnd1);
+        
+        SpringJoint newJoint1 = SetupJoint(newEnd1.rigidbody);
+        newJoint1.anchor = ends[1].localPosition;
+        newJoint1.connectedAnchor = ends[0].worldPosition;
+        
+        EndData newEnd2 = new EndData(hit2.rigidbody, hit2.rigidbody.transform.InverseTransformPoint(hit2.point));
+        ends.Insert(2, newEnd2);
+        
+        SpringJoint newJoint2 = SetupJoint(newEnd2.rigidbody);
+        newJoint2.anchor = ends[2].localPosition;
+        newJoint2.connectedAnchor = ends[3].worldPosition;
+
+
+        _foundHit = true;
     }
 
     private void UpdateDynamicIndexes()
@@ -74,6 +118,7 @@ public class Rope : MonoBehaviour
 
         if (ends[0].rigidbody)
         {
+            
             if (ends[1].rigidbody)
             {
                 SpringJoint joint = SetupJoint(ends[1].rigidbody);
@@ -94,6 +139,7 @@ public class Rope : MonoBehaviour
         else
         {
             if (!ends[1].rigidbody) return;
+            
             SpringJoint joint = SetupJoint(ends[1].rigidbody);
 
             joint.anchor = ends[1].localPosition;
@@ -110,8 +156,8 @@ public class Rope : MonoBehaviour
         joint.maxDistance = PlayerData.Instance.maxRopeDistance;
         joint.minDistance = PlayerData.Instance.minRopeDistance;
                     
-        joint.spring = PlayerData.Instance.springSpringiness;
-        joint.damper = PlayerData.Instance.springDamper;
+        joint.spring = PlayerData.Instance.springSpringiness * rb.mass;
+        joint.damper = PlayerData.Instance.springDamper * rb.mass;
         joint.massScale = PlayerData.Instance.springMassScale;
 
         return joint;
