dc63c4882b0fc52a72b8768edb3e7165c3f7b4b5
Harits Nur Fauzan <mharitsnf@gmail.com>
2023-03-01 12:02:05 +0100

    Add materials, tweak particles, add mechanics prefab

--- Assets/Scripts/Mechanics/Objects/ObjectMechanicsController.cs
+++ Assets/Scripts/Mechanics/Objects/ObjectMechanicsController.cs
@@ -1,31 +1,44 @@
 using System;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Serialization;
 
 public class ObjectMechanicsController : MonoBehaviour
 {
+    private GameObject _parent;
+    
     private Rigidbody _rb;
+    private MeshRenderer _meshRenderer;
+    public Material[] materials;
     
     private void Awake()
     {
-        gameObject.tag = "Object";
-
         try
         {
-            Assert.IsTrue(GetComponent<Rigidbody>());
-            Assert.IsTrue(GetComponent<Collider>());
+            _parent = transform.parent.gameObject;
         }
         catch (Exception)
         {
             Debug.Break();
-            throw;
+            return;
         }
-
-        _rb = GetComponent<Rigidbody>();
+        
+        gameObject.tag = "Object";
+        _parent.tag = "Object";
+        
+        _rb = _parent.GetComponent<Rigidbody>();
+        _meshRenderer = _parent.GetComponent<MeshRenderer>();
+        
+        SetMaterial(0);
     }
 
+    private void SetMaterial(int index)
+    {
+        _meshRenderer.material = materials[index];
+    }
+    
     private void LateUpdate()
     {
-        if (GetComponents<SpringJoint>().Length == 0) _rb.freezeRotation = false;
+        if (_parent.GetComponents<SpringJoint>().Length == 0) _rb.freezeRotation = false;
     }
 }
